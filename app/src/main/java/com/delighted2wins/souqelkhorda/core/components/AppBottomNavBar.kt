d3a25a85eda726ed041e14cfe1b890bd4e8c67f4
Ahmed Saad <medosaad2071@gmail.com>
2025-09-14 01:07:41 +0300

    removed unnecessary imports

--- app/src/main/java/com/delighted2wins/souqelkhorda/core/components/AppBottomNavBar.kt
+++ app/src/main/java/com/delighted2wins/souqelkhorda/core/components/AppBottomNavBar.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
