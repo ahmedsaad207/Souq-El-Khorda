e9e28efa21869b2143a5077c458a69cb35f19dd6
Ali Kotb <AliKotb38@gmail.com>
2025-09-26 23:08:48 +0300

    feat(buyers): Implement buyer registration and fetching logic
    This commit introduces the necessary components for buyer functionality, including:
    
    - **BuyerState:** Defines states for buyer-related operations (Loading, RegisterSuccess, SuccessLoading, Error, Idle).
    - **BuyerViewModel:** Manages buyer registration, fetching nearest buyers, and checking if a user is a buyer. It handles UI state and interactions with use cases.
    - **BuyerRemoteDataSource:** Implements data fetching and storage for buyers using Firebase Firestore.
    - **BuyerRepo:** Repository implementation for buyer-related data operations, delegating to the remote data source.

---
+++ app/src/main/java/com/delighted2wins/souqelkhorda/features/buyers/presentation/view_model/BuyerViewModel.kt
@@ -0,0 +1,106 @@
+package com.delighted2wins.souqelkhorda.features.buyers.presentation.view_model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.delighted2wins.souqelkhorda.core.enums.AuthMsgEnum
+import com.delighted2wins.souqelkhorda.features.authentication.domain.useCase.GetCashUserCase
+import com.delighted2wins.souqelkhorda.features.buyers.data.model.toBuyerDto
+import com.delighted2wins.souqelkhorda.features.buyers.domain.use_case.GetBuyersCase
+import com.delighted2wins.souqelkhorda.features.buyers.domain.use_case.IsBuyersCase
+import com.delighted2wins.souqelkhorda.features.buyers.domain.use_case.RegisterBuyersCase
+import com.delighted2wins.souqelkhorda.features.buyers.presentation.state.BuyerState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BuyerViewModel @Inject constructor(
+    val registerBuyersCase: RegisterBuyersCase,
+    val getBuyersCase: GetBuyersCase,
+    val isBuyersCase: IsBuyersCase,
+    val getUserCase: GetCashUserCase
+) : ViewModel() {
+    private val _registerState = MutableStateFlow<BuyerState>(BuyerState.Idle)
+    val registerState = _registerState.asStateFlow()
+
+    private val _nearestBuyers = MutableStateFlow<BuyerState>(BuyerState.Loading)
+    val nearestBuyers = _nearestBuyers.asStateFlow()
+    private val _isBuyerState = MutableStateFlow<Boolean>(false)
+    val isBuyerState = _isBuyerState.asStateFlow()
+    private val _message = MutableSharedFlow<String>()
+    val message = _message.asSharedFlow()
+    fun registerBuyer(latitude: Double, longitude: Double, scrapTypes: List<String>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (latitude == 0.0 || longitude == 0.0) {
+                _message.emit(AuthMsgEnum.LOCATIONEMPTY.getMsg())
+                return@launch
+            }
+            if (scrapTypes.isEmpty()) {
+                _message.emit(AuthMsgEnum.SCRAPLISTEMPTY.getMsg())
+                return@launch
+            }
+            registerBuyersCase(
+                getUserCase().toBuyerDto(
+                    latitude,
+                    longitude,
+                    scrapTypes
+                )
+            ).catch {
+                _message.emit(AuthMsgEnum.UNAUTHORIZED.getMsg())
+            }.collect { state ->
+                _registerState.emit(state)
+                when (state) {
+                    is BuyerState.RegisterSuccess -> {
+                        val user = state.buyerDto
+                        _message.emit(AuthMsgEnum.SIGNUPSUCCESS.getMsg())
+                    }
+                    is BuyerState.Error -> {
+                        _message.emit(AuthMsgEnum.UNAUTHORIZED.getMsg())
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    fun getNearstBuyers() {
+        viewModelScope.launch(Dispatchers.IO) {
+            getBuyersCase().catch {
+                _message.emit(AuthMsgEnum.UNAUTHORIZED.getMsg())
+            }.collect { state ->
+                when (state) {
+                    is BuyerState.SuccessLoading -> {
+                        val user = getUserCase()
+                        val filteredBuyers = state.list.filter { buyer ->
+                            buyer.governorate == user.governorate && buyer.buyerID!=user.id
+                        }
+                        _nearestBuyers.emit(BuyerState.SuccessLoading(filteredBuyers))
+                    }
+                    is BuyerState.Error -> {
+                        _nearestBuyers.emit(state)
+                        _message.emit(AuthMsgEnum.UNAUTHORIZED.getMsg())
+                    }
+                    else -> {
+                        _nearestBuyers.emit(state)
+                    }
+                }
+            }
+        }
+    }
+    fun isBuyer() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _isBuyerState.value = isBuyersCase(getUserCase().id)
+            } catch (e: Exception) {
+                _isBuyerState.value = false
+                _message.emit(AuthMsgEnum.UNAUTHORIZED.getMsg())
+            }
+        }
+    }
+}
