00f8e5f1d47b9e4ce85d59d35bab7de3f2c29406
Ali Kotb <AliKotb38@gmail.com>
2025-09-26 23:09:03 +0300

    feat(buyers): Implement buyer registration and fetching logic
    This commit introduces the necessary components for buyer functionality, including:
    
    - **BuyerState:** Defines states for buyer-related operations (Loading, RegisterSuccess, SuccessLoading, Error, Idle).
    - **BuyerViewModel:** Manages buyer registration, fetching nearest buyers, and checking if a user is a buyer. It handles UI state and interactions with use cases.
    - **BuyerRemoteDataSource:** Implements data fetching and storage for buyers using Firebase Firestore.
    - **BuyerRepo:** Repository implementation for buyer-related data operations, delegating to the remote data source.

--- app/src/main/java/com/delighted2wins/souqelkhorda/features/orderdetails/presentation/screen/CompanyOrderDetailsUI.kt
+++ app/src/main/java/com/delighted2wins/souqelkhorda/features/orderdetails/presentation/screen/CompanyOrderDetailsUI.kt
@@ -22,11 +22,11 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Receipt
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.outlined.Inventory2
-import androidx.compose.material3.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
