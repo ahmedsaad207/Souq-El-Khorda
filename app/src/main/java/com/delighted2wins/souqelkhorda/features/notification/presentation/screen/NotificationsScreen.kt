a889bf294c0607bb07e3e7d01ab47cc876670c43
Ali Kotb <AliKotb38@gmail.com>
2025-09-29 19:35:58 +0300

    fix(notifications): Replace deprecated Divider with HorizontalDivider

--- app/src/main/java/com/delighted2wins/souqelkhorda/features/notification/presentation/screen/NotificationsScreen.kt
+++ app/src/main/java/com/delighted2wins/souqelkhorda/features/notification/presentation/screen/NotificationsScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
